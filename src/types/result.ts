e1096d5bed3cf1d0083dc9e85450e490e52b453a
Mono5571 <sidedrop13ss090@gmail.com>
2026-02-20 15:24:59 +0900

    rm -g TS, pnpm init, add TS, install and modified .gitignore

--- src/types/result.ts
+++ src/types/result.ts
@@ -1,11 +1,13 @@
 type Success<D> = {
   readonly isSuccess: true;
   readonly data: D;
+  readonly error: never;
 };
 
 type Failure<E> = {
   readonly isSuccess: false;
   readonly error: E;
+  readonly data: never;
 };
 
 type Result<D, E> = Success<D> | Failure<E>;
