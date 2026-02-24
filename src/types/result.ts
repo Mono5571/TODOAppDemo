7839201c5cb8c85775617098e625a993171f7975
takumi <seven.eleven.tm@gmail.com>
2026-02-24 14:10:44 +0900

    ソースの軽微な修正

--- src/types/result.ts
+++ src/types/result.ts
@@ -1,13 +1,11 @@
 type Success<D> = {
   readonly isSuccess: true;
   readonly data: D;
-  readonly error: never;
 };
 
 type Failure<E> = {
   readonly isSuccess: false;
   readonly error: E;
-  readonly data: never;
 };
 
 type Result<D, E> = Success<D> | Failure<E>;
