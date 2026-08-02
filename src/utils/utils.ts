a071b644da7e5d5094b7e28f3422b76c8d043bc6
Mono5571 <sidedrop13ss090@gmail.com>
2026-08-02 16:47:58 +0900

    [Deleted] unused code: deleted defined but unused function isKey() and isTodoKey() / [Merged Codes] putted priority validation codes together.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,15 +2,8 @@
 export const isElement = <E extends string>(maybeElement: string, listOfElement: readonly E[]): maybeElement is E =>
   listOfElement.some((e) => e === maybeElement);
 
-export const isKey = <T extends { [key: string | number | symbol]: any }>(
-  key: string | number | symbol,
-  keyMap: T
-): key is keyof T => Object.hasOwn(keyMap, key);
-
 export const existsTrueVal = <T extends number | string | symbol>(obj: Record<T, boolean>): boolean =>
   Object.entries(obj).some(([_, v]) => v === true);
 
-export const shallowObjectEqual = <V extends { [k: string]: any }>(a: V, b: V): boolean => {
-  if (a === b) return true;
-  return Object.entries(a).every(([k, v]) => v === b[k]);
-};
+export const shallowObjectEqual = <V extends { [k: string]: any }>(a: V, b: V): boolean =>
+  a === b ? true : Object.entries(a).every(([k, v]) => v === b[k]);
