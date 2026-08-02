368df963f81167ec2302ff663eee9c43b9785afe
Mono5571 <sidedrop13ss090@gmail.com>
2026-08-02 16:42:11 +0900

    [Modified] package.json and __test__ directory: enabled to execute test, wrote test for createErrors() first.

--- src/__test__/sample.test.ts
+++ src/__test__/sample.test.ts
@@ -1,16 +1,15 @@
-import { test, describe, it } from 'node:test';
-import * as assert from 'node:assert';
+import { type TestContext, describe, it } from 'node:test';
 
 function add(a: number, b: number): number {
   return a + b;
 }
 
 describe('たし算のテスト', () => {
-  it('二数の和がもとめられているか', () => {
-    assert.strictEqual(add(1, 2), 3);
+  it('二数の和がもとめられているか', (t: TestContext) => {
+    t.assert.strictEqual(add(1, 2), 3);
   });
 
-  it('負のたし算ができるか', () => {
-    assert.strictEqual(add(-2, -2), -4);
+  it('負のたし算ができるか', (t: TestContext) => {
+    t.assert.strictEqual(add(-2, -2), -4);
   });
 });
